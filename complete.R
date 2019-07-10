9179fba8aa4321ee47e0c1070573d00269b4bfc9
patrickkabongo <cracciska@gmail.com>
2019-07-10 19:03:08 -0300

    The function 'complete' reads a directory full of files and reports the number of completely observed cases in each data file. The function returns a data frame where the first column is the name of the file and the second column is the number of complete cases.

---
+++ complete.R
@@ -0,0 +1,18 @@
+complete <- function(directory, id = 1:332){
+        specdatafiles <- as.character(dir(directory))
+        specdatapaths <- paste(directory, "/", specdatafiles, sep="")
+        j <- 1
+        idv <- c()
+        sumC <- c()
+        for (i in id) {
+                data <- read.csv(specdatapaths[i])
+                idv[j] <- i
+                sumC[j] <- sum(complete.cases(data))
+                j <- j + 1
+        }
+        
+        x <- cbind(idv,sumC)
+        colnames(x) <- c("id","nobs")
+        
+        x
+}
